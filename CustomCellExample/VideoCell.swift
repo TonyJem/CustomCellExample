dce1784870c23e918d9e02ed757b1e2d874e16fa
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-12 09:45:18 +0300

    Update VideoCell class with Outlets and Function

--- CustomCellExample/VideoCell.swift
+++ CustomCellExample/VideoCell.swift
@@ -1,5 +1,11 @@
 import UIKit
 
 class VideoCell: UITableViewCell {
-
+    @IBOutlet var videoImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    func createCell(for newVideo: Video){
+        videoImageView.image = newVideo.previewImage
+        titleLabel.text = newVideo.title
+    }
 }
