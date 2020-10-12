9cb2ada9d7eac4f2e04a91f8b7b6b72e4778ab7d
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-12 09:11:26 +0300

    Create class "Video"

---
+++ CustomCellExample/Video.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+class Video {
+    
+    var title: String
+    var previewImage: UIImage
+    
+    init(title: String, previewImage: UIImage){
+        self.title = title
+        self.previewImage = previewImage
+    }
+}
