ca6ae0df8b49c08846dff0aea079ad180535ec79
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-12 09:40:37 +0300

    Create Array with Data

--- CustomCellExample/RootViewController.swift
+++ CustomCellExample/RootViewController.swift
@@ -12,16 +12,42 @@ class RootViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         videos = createVideosArray()
-        videoListTableView.dataSource = self
-
+        createVideoListTable()
     }
     
     // MARK: - Methods
     func createVideosArray() -> [Video]{
         
+        var createdArray: [Video] = []
+        
+        let video1 = Video(title: "Video1", previewImage: UIImage(named: "Video01")!)
+        let video2 = Video(title: "Video2", previewImage: UIImage(named: "Video02")!)
+        let video3 = Video(title: "Video3", previewImage: UIImage(named: "Video03")!)
+        let video4 = Video(title: "Video4", previewImage: UIImage(named: "Video04")!)
+        let video5 = Video(title: "Video5", previewImage: UIImage(named: "Video05")!)
+        let video6 = Video(title: "Video6", previewImage: UIImage(named: "Video06")!)
+        let video7 = Video(title: "Video7", previewImage: UIImage(named: "Video07")!)
+        let video8 = Video(title: "Video8", previewImage: UIImage(named: "Video08")!)
+        let video9 = Video(title: "Video9", previewImage: UIImage(named: "Video09")!)
+        
+        createdArray.append(video1)
+        createdArray.append(video2)
+        createdArray.append(video3)
+        createdArray.append(video4)
+        createdArray.append(video5)
+        createdArray.append(video6)
+        createdArray.append(video7)
+        createdArray.append(video8)
+        createdArray.append(video9)
+        
+        return createdArray
     }
     
     // MARK: - TableSetup
+    func createVideoListTable() {
+        videoListTableView.dataSource = self
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return videos.count
     }
@@ -29,15 +55,11 @@ class RootViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         
-        
         <#code#>
     }
 
     
     
-    
-    
-
     
     
     // MARK: - Navigation
