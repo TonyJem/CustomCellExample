65d868ab5e9b7815f483e54c0b9e49c0c2a5d46f
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-12 09:19:50 +0300

    Start making RootViewController conform to UITableViewDataSource protocol

--- CustomCellExample/RootViewController.swift
+++ CustomCellExample/RootViewController.swift
@@ -1,23 +1,50 @@
 import UIKit
 
-class RootViewController: UIViewController {
-
+class RootViewController: UIViewController, UITableViewDataSource {
+    // MARK: - Outlets
+    @IBOutlet var videoListTableView: UITableView!
+    
+    // MARK: - Properties
+    
+    var videos: [Video] = []
+    
+    // MARK: - StartHere
     override func viewDidLoad() {
         super.viewDidLoad()
+        videos = createVideosArray()
+        videoListTableView.dataSource = self
 
     }
     
+    // MARK: - Methods
+    func createVideosArray() -> [Video]{
+        
+    }
+    
+    // MARK: - TableSetup
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        
+        <#code#>
+    }
 
     
     
     
     
+
     
     
+    // MARK: - Navigation
     
     
     /*
-    // MARK: - Navigation
+
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
